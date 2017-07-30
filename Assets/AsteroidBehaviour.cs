6143a7bf43212e8a7cd6f6cdfc2439efed8d1982
Ranzor <ranzor1@gmail.com>
2017-07-30 12:41:14 +0200

    Added Asteroids
    Added asteroids and implemented basic behaviour ship takes damage when hit by asteroids

---
+++ Assets/AsteroidBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidBehaviour : MonoBehaviour {
+
+    public GameObject shipStats;
+    public int damage;
+    public int speed;
+    private ShipStatsBehavior shipScript;
+    // Use this for initialization
+	void Start ()
+    {
+        shipScript = shipStats.GetComponent<ShipStatsBehavior>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(new Vector3 (-speed * Time.deltaTime,0,0), 0);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        shipScript.healt -= damage;
+        Destroy(gameObject);
+    }
+}
