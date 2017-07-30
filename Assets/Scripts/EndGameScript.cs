544abdba393d6083e8860c39afe4153a79f7af44
Ranzor <ranzor1@gmail.com>
2017-07-30 14:46:34 +0200

    Added game end stuff

---
+++ Assets/Scripts/EndGameScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameScript : MonoBehaviour
+{
+    ShipStatsBehavior shipStat;
+
+	void Start ()
+    {
+        shipStat = FindObjectOfType<ShipStatsBehavior>();
+	}
+	
+	void Update ()
+    {
+        if (shipStat.healt <= 0 || shipStat.energy <= 0)
+        {
+            Debug.Log("You losse");
+        }
+		
+	}
+}
